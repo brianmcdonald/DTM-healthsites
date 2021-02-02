b916041e84fc8abf4156ef627fde74e50e6eaa5d
Brian Mc Donald <brian@brianmcdonald.me>
2021-02-02 08:47:30 +0100

    added map

--- DTM-healthsites.py
+++ DTM-healthsites.py
@@ -18,10 +18,14 @@ import pandas as pd
 import requests
 import openpyxl
 import geopandas as gpd
+import folium 
+from shapely.ops import nearest_points
+from shapely.geometry import LineString
 
 # %%
 dtm = pd.read_excel("Mozambique - Site Assessment - Cyclone IDAI and Floods - Round 18.xlsx")
-dtm_short = dtm[['SSID','Latitude','Longitude']]
+dtm.rename(columns={'Latitude':'lat', 'Longitude':'lon'}, inplace=True)
+dtm_short = dtm[['SSID','lat','lon']]
 
 # %%
 r = requests.get('https://healthsites.io/api/v2/facilities/?api-key=8d978f6bfa9d7914751307b4ebcc500f711771d0&page=1&country=Mozambique')
@@ -40,8 +44,37 @@ for page in range(2,pagecount+2):
 # %%
 hsio_short = hsio[['osm_id','attributes.name','centroid.coordinates']]
 hsio_short['centroid.coordinates']
-t = pd.DataFrame(hsio_short['centroid.coordinates'].to_list(), columns=['lat', 'long'])
-hsio_short.join(t, how='outer').drop(['centroid.coordinates'], axis=1)
+t = pd.DataFrame(hsio_short['centroid.coordinates'].to_list(), columns=['lon', 'lat'])
+hsio_short = hsio_short.join(t, how='outer').drop(['centroid.coordinates'], axis=1)
+
 
 # %%
-len(dtm_short)
+def create_gdf(df, x="lon", y="lat"):
+    return gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df[x], df[y]), crs={"init":"epsg:4326"})
+
+hsio_short_gdf = create_gdf(hsio_short)
+dtm_short_gdf = create_gdf(dtm_short)
+
+# %%
+m = folium.Map([-20.0760232,34.3582913909869],
+               zoom_start=10,
+               tiles="CartoDb dark_matter")
+locs_dtm_short = zip(dtm_short_gdf.lat, dtm_short_gdf.lon)
+locs_hsio_short = zip(hsio_short_gdf.lat, hsio_short_gdf.lon)
+for location in locs_dtm_short:
+    folium.CircleMarker(location=location, color="red", radius=4).add_to(m)
+for location in locs_hsio_short:
+    folium.CircleMarker(location=location, color="white", radius=2).add_to(m)
+#m.save("map1.html")
+m
+
+
+# %%
+def calculate_nearest(row, destination, val, col="geometry"):
+    dest_unary = destination["geometry"].unary_union
+    nearest_geom = nearest_points(row[col], dest_unary)
+    match_geom = destination.loc[destination.geometry == nearest_geom[1]]
+    match_value = match_geom[val].to_numpy()[0]
+    return match_value
+
+points_gdf["nearest_geom"] = points_gdf.apply(calculate_nearest, destination=stations_gdf, val="geometry", axis=1)
